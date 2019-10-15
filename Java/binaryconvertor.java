ddc56e0fe3a0ddcd5f3785ccf1588bf1405ce304
Shivam Sheth <ssheth7@stevens.edu>
2019-10-15 13:44:25 -0400

    Working

--- Java/binaryconvertor.java
+++ Java/binaryconvertor.java
@@ -1,7 +1,9 @@
+//To do: take in binary number as String
 import java.util.Scanner;
 import java.math.BigInteger;
 import java.lang.Math;
 class binary{
+    //Uses recursion to check if the input was a binary number
     static boolean isBinary(long input){
 	if(input%10 ==1 && input%10 == 0)
 	    isBinary(input/10);
@@ -11,23 +13,25 @@ class binary{
 	    return true;
 	return true;
     }
+    //Uses the BigInteger class to account for large decimal numbers
     static BigInteger conBinary(long input){
 	long temp = input;
-	double power;
 	int place = 0;
 	BigInteger sum = new BigInteger("0");
+	BigInteger added= new BigInteger("2");
 	while(temp != 0){
-	    power = Math.pow(2.0,(double) temp%10);
-	    BigInteger added = new BigInteger("");
-	    added.valueOf((long) (power));
-	    sum.add(added);
-	temp/=10;
+	    if(temp % 10 == 1){
+		sum = sum.add(added.pow(place));
+	    }
+	place++;
+        temp/=10;
 	}
 	return sum;
 	}
     public static void main(String[] args){
 	long binary;
 	Scanner scanner = new Scanner(System.in);
+	while(true){
 	System.out.println("Enter a binary number: ");
 	while(true){
 	    if(!scanner.hasNextLong()){
@@ -41,8 +45,12 @@ class binary{
 		System.out.println("Not a binary number! Enter a binary number: ");
 	    }
 	}
-	System.out.println( binary + " in decimal is " + conBinary(binary)); 
-	
+	System.out.println(binary + " in decimal is " + conBinary(binary)); 
+	System.out.println("Would you like to convert another number? ");
+	String again = scanner.next();
+	if(again.indexOf("y") == -1)
+	    break;
+	}
     }
 
     
