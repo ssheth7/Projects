549b1868d93e4a04fa62ce33809e89edad4c921e
Shivam Sheth <ssheth7@stevens.edu>
2019-10-16 13:20:14 -0400

    Converts binary numbers of any length

--- Java/binaryconvertor.java
+++ Java/binaryconvertor.java
@@ -1,37 +1,42 @@
-//To do: take in binary number as String
-
+//Can take a binary number of any length and convert it to decimal
 import java.util.Scanner;
 import java.math.BigInteger;
-import java.lang.Math;
 class binary{
     //Uses recursion to check if the input was a binary number
-    static boolean isBinary(String input){
-	char lastnum = input.charAt(input.length()-1);
-	if(lastnum =='1' && lastnum == '0')
-	    isBinary(input.substring(0,input.length()-1));
-	else if(lastnum !='1' && lastnum !='0')
-	    return false;
-	else if (input.substring(0, input.length()-1).equals("1")|| input.substring(0, input.length()-1).equals('0'))
+    //if the last digit is 0 or 1, it calls itself again and uses input without its last digit
+    static boolean isBinary(String input){	
+	if(input.length() > 1){
+	    char lastnum = input.charAt(input.length()-1);
+            String withoutlast = input.substring(0, input.length()-1);
+	    if(lastnum =='1' || lastnum == '0')
+		return isBinary(withoutlast);
+	    else return false;
+	}
+	if(input.charAt(0)=='1' || input.charAt(0) == '0')
 	    return true;
-	return true;
+	return false;
     }
 
     
     //Uses the BigInteger class to account for large decimal numbers
+    //Checks if the last digit of the binary number is 1, if true, it adds 2^place to sum, if false, place is incremented and the binary number is divided by 10 
     static BigInteger conBinary(BigInteger input){
 	BigInteger temp = input;
 	int place = 0;
 	BigInteger sum = new BigInteger("0");
 	BigInteger added= new BigInteger("2");
-	while(!temp.toString().equals(0)){
-	    if(temp.divide(BigInteger.valueOf(10)).compareTo(new BigInteger("1"))== 0){
+	BigInteger one  = new BigInteger("1");
+	BigInteger ten = new BigInteger("10");
+	while(!temp.toString().equals("0")){
+	    if(temp.mod(ten).compareTo(one)== 0){
 		sum = sum.add(added.pow(place));
 	    }
 	place++;
-        temp= temp.divide(BigInteger.valueOf(10));
+        temp= temp.divide(ten);
 	}
 	return sum;
 	}
+    //tries to convert the string to a long, if it fails the function catches the exception and returns false, else it returns true
     static boolean isNumeric(String binary){
 	try {
 	    Double.parseDouble(binary);
@@ -41,7 +46,8 @@ class binary{
 	    return false;
 	}
     }
-    
+    //Takes the users input as a string called binary
+    //First checks if its numeric, checks if its binary, and then converts the String into a BigInteger
     public static void main(String[] args){
 	String binary;
 	Scanner scanner = new Scanner(System.in);
@@ -56,7 +62,7 @@ class binary{
 	BigInteger binarycon = new BigInteger(binary);
 	System.out.println(binary + " in decimal is " + conBinary(binarycon)); 
 	System.out.println("Would you like to convert another number? ");
-	String again = scanner.next();
+	String again = scanner.nextLine();
 	if(again.indexOf("y") == -1)
 	    break;
 	}
