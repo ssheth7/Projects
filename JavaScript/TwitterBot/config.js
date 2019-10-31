a495aad11b4d9b6aceee7db7c3f9f8cbb1cc6094
ssheth7 <54548775+ssheth7@users.noreply.github.com>
2019-10-31 15:44:02 -0400

    Delete config.js

--- JavaScript/TwitterBot/config.js
+++
@@ -1,5 +0,0 @@
-module.exports  = {
-    consumer key:'9zqKvxdkYkNeHYi8be0ySSAKq'
-    consumer secret:'Se1vMD5XQ9WGIzXdN6VN2PRU2n2FSHrG02oWOP3YOVpl6UuwMr'
-    access token:'1189977669624045568-AmEwXe1Yr73FYcvfu3nVsLYhRUjxDH'
-}
